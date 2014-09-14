fe2d670a15ba87b09e3afb07353ab99d946ff984
Christopher <eklundchristopher@gmail.com>
2014-09-14 04:26:10 +0200

    Typo for the PlayerSkill model

--- src/models/PlayerSkill.php
+++ src/models/PlayerSkill.php
@@ -1,6 +1,6 @@
 <?php namespace pandaac\Avesta;
 
-class PlayerSkill extends \pandaac\Foundation\Base {
+class PlayerSkill extends \pandaac\Foundation\Foundation {
 
 	/**
 	 * Override columns with their appropriate counterpart.
